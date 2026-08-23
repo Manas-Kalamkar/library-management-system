c7e1f842b9278d78770614f9ac51499fc5ca3777
Manas <manaskalamkar910@gmail.com>
2026-08-24 01:57:34 +0530

    feat: set up router of all api

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import express, { type Express } from "express";
 import cors from 'cors';
-import router from "./routes/book.route.js";
+import booksRouter from "./routes/book.route.js";
+import authorsRouter from "./routes/author.route.js";
+import librarianRouter from "./routes/librarian.route.js";
+import borrowerRouter from "./routes/borrower.route.js";
 
 const app: Express = express();
-
 app.use(express.json());
 app.use(cors());
 
-app.use('/', router)
+
+
+app.use('/books', booksRouter)
+app.use('/authors', authorsRouter)
+app.use('/librarians', librarianRouter)
+app.use('/borrowers', borrowerRouter)
 
 
 export default app;
