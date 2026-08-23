8978c0d39790255d85590724c0cb4e3653c52745
Manas <manaskalamkar910@gmail.com>
2026-08-24 02:03:32 +0530

    feat: routed request to author controllers

--- src/routes/author.route.ts
+++ src/routes/author.route.ts
@@ -1,15 +1,19 @@
 import { Router } from "express"
-import { getAuthorsController, getAuthorsByIdController,addAuthorController,deleteAuthorController } from "../controllers/author.controller.js"
+import { getAuthorsController, getAuthorByIdController,addAuthorController,deleteAuthorController,updateAuthorController } from "../controllers/author.controller.js"
+import { removeUndefinedMiddleware } from "../middlewares/removeUndefined.js"
+import { UpdateAuthor } from "../schemas/author.schema.js"
 
 
 const authorsRouter =  Router()
 
 
 authorsRouter.get("/",getAuthorsController)
-authorsRouter.get("/:id",getAuthorsByIdController)
+authorsRouter.get("/:id",getAuthorByIdController)
+
 authorsRouter.post("/",addAuthorController)
-authorsRouter.patch("/:id",addAuthorController)
 authorsRouter.delete("/:id",deleteAuthorController)
+
+authorsRouter.patch("/:id",removeUndefinedMiddleware(UpdateAuthor),updateAuthorController)
 
 
 export default authorsRouter
