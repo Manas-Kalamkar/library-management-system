82f0eb64389a25e57d1f82ede0985cd9e901e288
Manas <manaskalamkar910@gmail.com>
2026-08-29 23:59:07 +0530

    refactor: add try catch change books to borrowings feat(exception handing): add throw errors

--- src/services/borrowing.service.ts
+++ src/services/borrowing.service.ts
@@ -1,18 +1,22 @@
+import { Prisma } from "../generated/prisma/client.js";
 import type { RemoveUndefinedType } from "../middlewares/removeUndefined.js";
 import { addBorrowing, deleteBorrowing, getBorrowingById, getBorrowings, updateBorrowing } from "../repositories/borrowing.repository.js"
 import type { BorrowingQuerySchemaType, CreateBorrowingType, UpdateBorrowingType } from "../schemas/borrowing.schema.js";
+import { AppError } from "../utils/AppError.js";
 
 
 
-export const getBorrowingsService = async (query:BorrowingQuerySchemaType) => {
-    const books = await getBorrowings(query);
-    return books;
+export const getBorrowingsService = async (query: BorrowingQuerySchemaType) => {
+    const borrowings = await getBorrowings(query);
+    if (!borrowings.length) throw new AppError("Borrowings Not Found", 404)
+    return borrowings;
 }
 
 
 export const getBorrowingByIdService = async (id: string) => {
-    const books = await getBorrowingById(id);
-    return books;
+    const borrowing = await getBorrowingById(id);
+    if (!borrowing) throw new AppError("Borrowings Not Found", 404)
+    return borrowing;
 }
 
 // adding multiple borrowings in a array
@@ -21,7 +25,7 @@ export const getBorrowingByIdService = async (id: string) => {
 // ) => {
 //     const borrowings: Prisma.BorrowingCreateManyInput[] = data.map((item) => (
 //         {
-//             bookId: item.bookId,
+//             borrowingId: item.borrowingId,
 //             borrowerId: item.borrowerId,
 //             librarianId: item.librarianId,
 //             borrowedAt: item.borrowedAt,
@@ -37,29 +41,51 @@ export const getBorrowingByIdService = async (id: string) => {
 export const addBorrowingsService = async (
     data: CreateBorrowingType
 ) => {
-    let dueDate = data.dueDate ?? new Date(data.borrowedAt.getTime() + 7 * 24 * 60 * 60 * 1000)
 
-    const newData  = {
-        bookId: data.bookId,
-        librarianId: data.librarianId,
-        borrowerId : data.borrowerId,
-        borrowedAt : data.borrowedAt,
-        dueDate:dueDate,
-        returnedAt:data.returnedAt ?? null
+    try{
+
+        let dueDate = data.dueDate ?? new Date(data.borrowedAt.getDate() + 7 )    
+        const newData = {
+            bookId: data.bookId,
+            librarianId: data.librarianId,
+            borrowerId: data.borrowerId,
+            borrowedAt: data.borrowedAt,
+            dueDate: dueDate,
+            returnedAt: data.returnedAt ?? null
+        }
+        const borrowing = await addBorrowing(newData);
+    
+        return borrowing
+    }catch(error){
+        if(error instanceof Prisma.PrismaClientKnownRequestError){
+            if(error.code === "P2002") throw new AppError("Borrowing already exists",409)
+        }
     }
-    const borrowing = await addBorrowing(newData);
-
-    return borrowing
 };
 
 
 export const updateBorrowingService = async (id: string, data: RemoveUndefinedType<UpdateBorrowingType>) => {
-    const book = await updateBorrowing(id, data);
-    return book;
+    try{
+        const borrowing = await updateBorrowing(id, data);
+        return borrowing;
+    }catch(error){
+        if(error instanceof Prisma.PrismaClientKnownRequestError){
+            if(error.code === "P2002") throw new AppError("Borrowing already exists",409)
+            if(error.code === "P2025") throw new AppError("Borrowing not found",404)
+        }
+    }
 }
 
 
 export const deleteBorrowingsService = async (id: string) => {
-    const book = await deleteBorrowing(id);
-    return book;
+    try{
+        const borrowing = await deleteBorrowing(id);
+        return borrowing;
+    }catch(error){
+        if(error instanceof Prisma.PrismaClientKnownRequestError){
+            
+            if(error.code === "P2003") throw new AppError("Borrowing cannot be deleted because related records exits",409)
+            if(error.code === "P2025") throw new AppError("Borrowing not found",404)
+        }
+    }
 }
