60cde3532dafd10f6f073f3476132322780e24e8
Manas <manaskalamkar910@gmail.com>
2026-09-02 23:47:04 +0530

    feat: add session in app

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,40 @@
-import express, { type Express } from "express";
+import express from "express";
+import type { Express, Request, Response } from "express";
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+
+import homeRouter from "./routes/home.route.js";
 import booksRouter from "./routes/book.route.js";
 import authorsRouter from "./routes/author.route.js";
 import librarianRouter from "./routes/librarian.route.js";
 import borrowerRouter from "./routes/borrower.route.js";
 import borrowingRouter from "./routes/borrowing.route.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import type { Request, Response } from "express";
+
 import { AppError } from "./utils/AppError.js";
+import userRouter from "./routes/user.route.js";
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser("helloworld"));
+app.use(session({
+    secret:'manas the great',
+    resave:false,
+    saveUninitialized:false,
+    cookie:{maxAge:30000}
+}))
 
 
+app.use('/api/auth',userRouter)
 
-app.use('/books', booksRouter)
-app.use('/authors', authorsRouter)
-app.use('/librarians', librarianRouter)
-app.use('/borrowers', borrowerRouter)
-app.use('/borrowings', borrowingRouter)
+app.use('/', homeRouter)
+app.use('/api/books', booksRouter)
+app.use('/api/authors', authorsRouter)
+app.use('/api/librarians', librarianRouter)
+app.use('/api/borrowers', borrowerRouter)
+app.use('/api/borrowings', borrowingRouter)
 
 app.use((req: Request, res: Response) => {
     throw new AppError("Route Not Found", 404)
@@ -32,7 +47,7 @@ process.on("uncaughtException", (err) => {
     process.exit(1)
 })
 
-process.on("unhandledRejection", (err:Error) => {
+process.on("unhandledRejection", (err: Error) => {
     console.log("Uncaught Rejection:", err.message)
     process.exit(1)
 })
