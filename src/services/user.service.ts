7250a393efe5ef723848a0596a0bde5efc8b83a2
Manas <manaskalamkar910@gmail.com>
2026-09-03 18:20:30 +0530

    feat: find the email in db and then password

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,22 +1,34 @@
-import { addUser, deleteUser, findUser, findUserById } from "../repositories/user.repository.js";
+import { addUser , deleteUser, findUser, findUserByEmail, findUserById } from "../repositories/user.repository.js";
 import type { LoginDataType, SignupDataType } from "../schemas/user.schema.js";
-
+import { hashPassword, comparePassword } from "../middlewares/hashPassword.js";
+import { AppError } from "../utils/AppError.js";
 
 export const userSignupService = async (data: SignupDataType) => {
+    data.password = await hashPassword(data.password)
 
     return await addUser(data);
 }
 
-export const userLoginService = async (data: LoginDataType) => {
-    return await findUser(data);
+export const userLoginService = async ({ email, password }: LoginDataType) => {
+    const user = await findUserByEmail(email)
+    if (!user) throw new AppError("User Not Found ", 404)
+    const isPasswordCorrect = await (comparePassword(password, user.password))
+
+
+    if (!isPasswordCorrect) throw new AppError("Password Incorrect", 403)
+
+    return user;
+
 }
+ 
+
 export const userDeleteService = async (data: LoginDataType) => {
 
     return await deleteUser(data);
 }
 
-export const userStatusService = async (id:string) => {
-    const user =  await findUserById(id);
+export const userStatusService = async (id: string) => {
+    const user = await findUserById(id);
     return user;
 
 }
