acdbe6029cf302755a52f05b793682fe856a838e
Manas <manaskalamkar910@gmail.com>
2026-09-09 23:56:11 +0530

    feat: middleware handles auth

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError.js";
+
+
+
+export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.session.userId) throw new AppError("Unauthorized. Please login first", 401)
+
+    next()
+}
+
+export const requireRole = (allowedRole:("LIBRARIAN" | "BORROWER")[]) => {
+
+    return (req: Request, res: Response, next: NextFunction) => {
+        const userRole = req.session.role;
+
+        if(!userRole || !allowedRole.includes(userRole  )) throw new AppError("Forbidden: You do not have permission to perform this action.",403)
+        next()
+    }
+}
