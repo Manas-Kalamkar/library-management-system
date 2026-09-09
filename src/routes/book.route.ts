0360fc21b53c7f3cc17c8f670c37292000d0c2c6
Manas <manaskalamkar910@gmail.com>
2026-09-10 00:00:40 +0530

    feat: check add for auth of user

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -2,15 +2,16 @@ import { Router } from "express"
 import { addBookController, deleteBookController, getBooksByIdController, getBooksController, updateBookController } from "../controllers/book.controller.js";
 import { removeUndefinedMiddleware } from "../middlewares/removeUndefined.js";
 import { UpdateBook } from "../schemas/book.schema.js";
+import { requireAuth, requireRole } from "../middlewares/auth.js";
 
 const booksRouter = Router();
 
-booksRouter.get("/", getBooksController)
-booksRouter.post("/", addBookController)
+booksRouter.get("/", requireAuth, getBooksController)
+booksRouter.post("/",requireAuth,requireRole(["LIBRARIAN"]), addBookController)
 
 
-booksRouter.get("/:id", getBooksByIdController)
-booksRouter.patch("/:id", removeUndefinedMiddleware(UpdateBook), updateBookController)
-booksRouter.delete("/:id", deleteBookController)
+booksRouter.get("/:id",requireAuth, getBooksByIdController)
+booksRouter.patch("/:id",requireAuth,requireRole(["LIBRARIAN"]), removeUndefinedMiddleware(UpdateBook), updateBookController)
+booksRouter.delete("/:id", requireAuth,requireRole(["LIBRARIAN"]),deleteBookController)
 
 export default booksRouter;
