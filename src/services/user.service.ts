410e6be36cdfa369eacb515334def83fca76c8a7
Manas <manaskalamkar910@gmail.com>
2026-09-10 23:59:51 +0530

    feat: add error if already user exist

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,22 @@
-import { addUser , deleteUser, findUser, findUserByEmail, findUserById } from "../repositories/user.repository.js";
+import { addUser, deleteUser, findUser, findUserByEmail, findUserById } from "../repositories/user.repository.js";
 import type { LoginDataType, SignupDataType } from "../schemas/user.schema.js";
 import { hashPassword, comparePassword } from "../middlewares/hashPassword.js";
 import { AppError } from "../utils/AppError.js";
+import { Prisma } from "../generated/prisma/client.js";
 
 export const userSignupService = async (data: SignupDataType) => {
-    const user= await findUserByEmail(data.email)
 
-
-    if(user) throw new AppError("User already exists",409)
-        
     data.password = await hashPassword(data.password)
+    try {
+        const user = await addUser(data)
+        return user;
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
 
-    return await addUser(data);
+            if (error.code === "P2002") throw new AppError(`User already exists.`, 409)
+        }
+    }
+
 }
 
 export const userLoginService = async ({ email, password }: LoginDataType) => {
@@ -23,27 +28,27 @@ export const userLoginService = async ({ email, password }: LoginDataType) => {
     if (!isPasswordCorrect) throw new AppError("Invalid Credentials ", 403)
 
     return {
-        id:user.id,
-        email:user.email,
-        userName:user.userName,
-        role:user.role
+        id: user.id,
+        email: user.email,
+        userName: user.userName,
+        role: user.role
     };
 
 }
- 
 
-export const userDeleteService = async (data: LoginDataType) => {
 
-    return await deleteUser(data);
+export const userDeleteService = async (id:string) => {
+
+    return await deleteUser(id);
 }
 
 export const userStatusService = async (id: string) => {
     const user = await findUserById(id);
 
-    if(!user) throw new AppError('User not Found',404)
-    return  {
-        id:user.id,
-        email:user.email,
-        userName:user.userName
+    if (!user) throw new AppError('User not Found', 404)
+    return {
+        id: user.id,
+        email: user.email,
+        userName: user.userName
     };
 }
