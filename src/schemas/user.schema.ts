1b82d361fcc88567b879e022636a8feee3876036
Manas <manaskalamkar910@gmail.com>
2026-09-11 00:01:45 +0530

    feat: add phoneNo of user while signin

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,7 +3,8 @@ import * as z from 'zod'
 export const SignupData = z.object({
     userName: z.string(),
     email: z.email(),
-    password: z.coerce.string().min(8).max(15)
+    password: z.coerce.string().min(8).max(15),
+    phoneNo:z.coerce.string().min(10).max(10)
 })
 
 export type SignupDataType = z.infer<typeof SignupData>
